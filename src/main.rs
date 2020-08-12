2399b65cb0346c1bd5eb86a98d970b3ddc166f5a
ArtHome12 <_mag12@yahoo.com>
2020-08-12 16:52:22 +0700

    Register and unregister with database

--- src/main.rs
+++ src/main.rs
@@ -27,14 +27,13 @@ pub static DB: OnceCell<tokio_postgres::Client> = OnceCell::new();
 #[derive(BotCommand)]
 #[command(rename = "lowercase", description = "Поддерживаются команды:")]
 enum Command {
-   #[command(description = "отправьте сообщение или зарегистрируйте свой чат.")]
    Start,
    #[command(description = "выводит этот текст.")]
    Help,
-   #[command(description = "регистрация нового чата, например '/register @your_chat', бот должен быть добавлен в этот чат, иначе он не сможет отправлять сообщения. Вы можете быть администратором только одного чата, при регистрации нового предыдущий будет забыт.")]
+   #[command(description = "регистрация новой публичной группы, например для группы t.me/your_chat надо отправить '/register @your_chat', бот должен быть добавлен в этот чат, иначе он не сможет отправлять сообщения. Вы можете быть администратором только одного чата, при регистрации нового предыдущий будет забыт.")]
    Register(String),
-   #[command(description = "указание боту забыть чат, например '/unregister @your_chat'.")]
-   Unregister(String),
+   #[command(description = "указание боту забыть чат, ранее вами зарегистрированный.")]
+   Unregister,
 }
 
 async fn answer(cx: UpdateWithCx<Message>, command: Command) -> ResponseResult<()> {
@@ -42,20 +41,22 @@ async fn answer(cx: UpdateWithCx<Message>, command: Command) -> ResponseResult<(
       Command::Start => cx.answer("Добро пожаловать. Отправьте сообщение, выберите чат из списка зарегистрированных в боте и оно будет направлено на модерацию администратору чата (он не будет знать, от кого). Если администратор одобрит его публикацию, сообщение будет отправлено ботом в чат также анонимно. Все поддерживаемые команды: /help").send().await?,
       Command::Help => cx.answer(Command::descriptions()).send().await?,
       Command::Register(chat_name) => {
-         let res = if chat_name.is_empty() {String::from("После команды /start надо указать имя чата, например если имя вашего чата @your_chat, то введите вручную и отправьте отдельным сообщением /register @your_chat")}
+         let res = if chat_name.is_empty() {String::from("После команды /register надо указать имя чата, например если имя вашего чата @your_chat, то введите вручную и отправьте отдельным сообщением /register @your_chat")}
          else {
             if &chat_name[..1] != "@" {format!("Имя чата должно начинаться со знака @, а вы ввели '{}'", chat_name)}
             else {
                // Пробуем отправить приветственное сообщение в чат
-               let chat_id = ChatId::ChannelUsername(chat_name);
+               let chat_id = ChatId::ChannelUsername(chat_name.clone());
                let res = cx.bot
-               .send_message(chat_id, "Приветствую вас. Я бот-анонимайзер, напишите мне в личку, я от своего имени перешлю сообщение админу и если он одобрит, я от своего имени перешлю его сюда и никто, кроме автора, не будет знать, от кого оно")
+               .send_message(chat_id, "Приветствую вас. Я бот-анонимайзер, напишите мне в личку, я от своего имени перешлю сообщение админу и если он одобрит, я от своего имени перешлю его сюда и никто, кроме вас самого, не будет знать, от кого оно")
                .send()
                .await;
                match res {
                   Ok(_) => {
                      // Всё хорошо, сохраним регистрацию
-                     String::from("Регистрация успешна")
+                     let user_id = cx.update.from().unwrap().id;
+                     db_register(user_id, chat_name).await;
+                     String::from("Регистрация успешна. Если бот не сможет отправить сообщение в чат или его услугами не будут пользоваться более 3-х месяцев, информация о нём будет стёрта, но вы всегда сможете зарегистрировать его заново")
                   }
                   Err(e) => format!("Не удалось отправить сообщение в чат, возможно вы забыли меня в него добавить: {}", e)
                }
@@ -63,8 +64,19 @@ async fn answer(cx: UpdateWithCx<Message>, command: Command) -> ResponseResult<(
          };
          cx.answer_str(res).await?
       }
-      Command::Unregister(chat_name) => {
-         cx.answer_str(format!("Your chat_name is @{}.", chat_name)).await?
+      Command::Unregister => {
+         let user_id = cx.update.from().unwrap().id;
+
+         // Проверим, что какой-нибудь чат был зарегистрирован
+         let res = match db_user_chat_name(user_id).await {
+            Some(chat_name) => {
+               // Удаляем чат и сообщаем об этом
+               db_unregister(user_id).await;
+               format!("Информация о чате {} удалена", chat_name)
+            }
+            None => String::from("Зарегистрированного вами чата не числится, если вы его регистрировали, то возможно он был удалён автоматически при ошибке или из-за долгого бездействия")
+         };
+         cx.answer_str(res).await?
       }
    };
 
@@ -139,7 +151,6 @@ async fn run() {
    log::info!("Starting cognito_bot...");
 
    let bot = Bot::from_env();
-   let cloned_bot = bot.clone();
 
    // Логин к БД
    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL env variable missing");    
@@ -162,7 +173,10 @@ async fn run() {
       _ => log::info!("Something wrong with database"),
    }
 
-   teloxide::commands_repl_with_listener(bot, "cognito_bot", answer, webhook(cloned_bot).await).await;
+   // Создадим таблицу в БД, если её ещё нет
+   check_database().await;
+
+   teloxide::commands_repl_with_listener(bot.clone(), "cognito_bot", answer, webhook(bot).await).await;
 }
 
 /// Создаёт таблицу, если её ещё не существует
@@ -179,11 +193,41 @@ async fn check_database() {
          CREATE TABLE users (
             PRIMARY KEY (user_id),
             user_id        INTEGER        NOT NULL,
-            user_name      VARCHAR(100)   NOT NULL,
-            contact        VARCHAR(100)   NOT NULL,
-            address        VARCHAR(100)   NOT NULL,
-            last_seen      TIMESTAMP      NOT NULL,
-         ", &[]).await.unwrap();
+            chat_name      VARCHAR(100)   NOT NULL,
+            last_use       TIMESTAMP      NOT NULL
+         )", &[]).await.unwrap();
    }
 }
 
+/// Регистрация чата для пользователя
+async fn db_register(user_id: i32, chat_name: String) {
+   let client = DB.get().unwrap();
+
+   // Удалим прежнюю информацию, если пользователь уже регистрировал чат
+   db_unregister(user_id).await;
+
+   // Добавляем новую запись, при ошибке сообщение в лог
+   if let Err(e) = client.execute("INSERT INTO chats (user_id, chat_name, last_use) VALUES ($1::INTEGER, $2::VARCHAR(100), NOW())", &[&user_id, &chat_name]).await {
+      log::error!("db_register({}, {}): {}", user_id, chat_name, e);
+   }
+}
+
+/// Удаление инормации о пользователе
+async fn db_unregister(user_id: i32) {
+   let client = DB.get().unwrap();
+   
+   // Выполняем запрос для удаления записи, при ошибке сообщение в лог
+   if let Err(e) = client.execute("DELETE FROM chats WHERE user_id = $1::INTEGER", &[&user_id]).await {
+      log::error!("db_unregister({}): {}", user_id, e);
+   }
+}
+
+/// Возвращает название чата для указанного пользователя
+async fn db_user_chat_name(user_id: i32) -> Option<String> {
+   let client = DB.get().unwrap();
+   let res = client.query_one("SELECT chat_name FROM chats WHERE user_id = $1::INTEGER", &[&user_id]).await;
+   match res {
+      Ok(data) => Some(data.get(0)),
+      _ => None,
+   }
+}
