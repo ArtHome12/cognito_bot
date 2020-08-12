86a93a2b856f069a65bc12b4a8b443ade459cbd9
ArtHome12 <_mag12@yahoo.com>
2020-08-12 12:15:06 +0700

    Change polling on listener

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,16 @@ http://www.gnu.org/licenses/gpl-3.0.html
 Copyright (c) 2020 by Artem Khomenko _mag12@yahoo.com.
 =============================================================================== */
 
-use teloxide::{prelude::*, utils::command::BotCommand};
+use teloxide::{
+   dispatching::update_listeners,
+   prelude::*,
+   utils::command::BotCommand
+};
+use std::{convert::Infallible, env, net::SocketAddr};
+use tokio::sync::mpsc;
+use warp::Filter;
+use reqwest::StatusCode;
+
 
 #[derive(BotCommand)]
 #[command(rename = "lowercase", description = "These commands are supported:")]
@@ -39,12 +48,71 @@ async fn main() {
     run().await;
 }
 
+async fn handle_rejection(error: warp::Rejection) -> Result<impl warp::Reply, Infallible> {
+   log::error!("Cannot process the request due to: {:?}", error);
+   Ok(StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+pub async fn webhook<'a>(bot: Bot) -> impl update_listeners::UpdateListener<Infallible> {
+   // Heroku defines auto defines a port value
+   let teloxide_token = env::var("TELOXIDE_TOKEN").expect("TELOXIDE_TOKEN env variable missing");
+   let port: u16 = env::var("PORT")
+      .expect("PORT env variable missing")
+      .parse()
+      .expect("PORT value to be integer");
+   // Heroku host example .: "heroku-ping-pong-bot.herokuapp.com"
+   let host = env::var("HOST").expect("have HOST env variable");
+   let path = format!("bot{}", teloxide_token);
+   let url = format!("https://{}/{}", host, path);
+
+   bot.set_webhook(url)
+      .send()
+      .await
+      .expect("Cannot setup a webhook");
+   
+   let (tx, rx) = mpsc::unbounded_channel();
+
+   let server = warp::post()
+      .and(warp::path(path))
+      .and(warp::body::json())
+      .map(move |json: serde_json::Value| {
+         let try_parse = match serde_json::from_str(&json.to_string()) {
+               Ok(update) => Ok(update),
+               Err(error) => {
+                  log::error!(
+                     "Cannot parse an update.\nError: {:?}\nValue: {}\n\
+                     This is a bug in teloxide, please open an issue here: \
+                     https://github.com/teloxide/teloxide/issues.",
+                     error,
+                     json
+                  );
+                  Err(error)
+               }
+         };
+         if let Ok(update) = try_parse {
+               tx.send(Ok(update))
+                  .expect("Cannot send an incoming update from the webhook")
+         }
+
+         StatusCode::OK
+      })
+      .recover(handle_rejection);
+
+   let serve = warp::serve(server);
+
+   let address = format!("0.0.0.0:{}", port);
+   tokio::spawn(serve.run(address.parse::<SocketAddr>().unwrap()));
+   rx
+}
+
+
 async fn run() {
     teloxide::enable_logging!();
     log::info!("Starting simple_commands_bot...");
 
     let bot = Bot::from_env();
+    let cloned_bot = bot.clone();
 
-    teloxide::commands_repl(bot, "cognito_bot", answer).await;
+    teloxide::commands_repl_with_listener(bot, "cognito_bot", answer, webhook(cloned_bot).await).await;
 }
 
