bfb6e1a882f7462aeedb2394ed7fa8a50325daec
Артем <_mag12@yahoo.com>
2021-03-15 15:13:58 +0700

    1

--- src/main.rs
+++ src/main.rs
@@ -182,7 +182,7 @@ async fn run() {
    let bot = Bot::from_env();
 
    // Логин к БД
-   let database_url = env::var("DATABASE_URL").expect("DATABASE_URL env variable missing") + " sslmode=require";
+   let database_url = env::var("DATABASE_URL").expect("DATABASE_URL env variable missing");
    log::info!("{}", database_url);
 
    let connector = TlsConnector::builder()
